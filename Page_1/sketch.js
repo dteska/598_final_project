0d4ac06ce7ebb29b5ea1854b0f27e3215f2dee59
Lulu Xiao <Lulu.xiao21@gmail.com>
2017-03-02 19:35:33 -0800

    Adding new page and parameters

--- Page_1/sketch.js
+++ Page_1/sketch.js
@@ -3,35 +3,41 @@
 // This creates the first page of our final HCDE program. The user answers a question to be
 // advanced to another state. 
 
+//TO-DO
+//Add functionality for buttons
+
+var mesX = 100;
+var mesY = 300;
+var butX = 360;
+var butY = 360;
+
 function setup() {
-  createCanvas (1200, 700);
+  createCanvas(1200, 700);
 }
 
 function draw() {
-  background (230);
-  message ();
-  buttons(); 
+  background(230);
+  message(mesX, mesY);
+  buttons(butX, butY);
 }
 
 // This is the function for the text shown on the page.
-function message (){
+function message() {
   textSize(40);
-  fill (0, 102, 153);
+  fill(0, 102, 153);
   textStyle(BOLD);
-  text ("Do you want to pop some bubbles to see a message?", 100, 300);
+  text("Do you want to pop some bubbles to see a message?", mesX, mesY);
 }
 
 // This makes the buttons
-function buttons(){
-  fill (74, 77, 81);
+function buttons() {
+  fill(74, 77, 81);
   noStroke();
-  rect(360, 360, 220, 50, 10);
-  rect(610, 360, 220, 50, 10);
+  rect(butX, butY, 220, 50, 10);
+  rect(butX + 250, butY, 220, 50, 10);
   fill(255);
   textSize(20);
   textStyle(NORMAL);
-  text("Yes, definitely!", 400, 390);
-  text ("No, not feeling it", 645, 390);
+  text("Yes, definitely!", butX + 40, butY + 30);
+  text("No, not feeling it", butX + 285, butY + 30);
 }
-
-//NEED TO ADD FUNCTIONALITY SO THAT BUTTON SENDS USER TO CORRECT PAGE.
