f09392e3af855db12cf9c2ad7231685af8816362
Lulu Xiao <Lulu.xiao21@gmail.com>
2017-03-03 22:35:22 -0800

    Updating page links

--- Project_combined/sketch.js
+++ Project_combined/sketch.js
@@ -1,24 +1,26 @@
 // Created by Danielle Teska and Lulu Xiao
-// Last edited Thursday, March 2, 2017
-// This creates the first page of our final HCDE program. The user answers a question to be
-// advanced to another state. 
+// Last edited Friday, March 3, 2017
+// This is the final project for our HCDE 598 class. The user choose whether they want to see a message or 
+// answer a question. If the user chooses yes, the user is directed to a page where they can pop bubbles to see
+// the message. If the user chooses no, the user is directed to a differnt page. They are allowed
+// to change their mind if they want, however. 
 
 //TO-DO
-//Add functionality for buttons. 
-//Add in image of Octocat or change background? 
+// Add in functionality for the buttons. 
+// Make everything prettier...
 
-// This stuff pertains to the first page. 
+var page = 0
+  // This stuff pertains to the first page. 
 var mesX = 100;
 var mesY = 300;
 var butX = 360;
 var butY = 360;
 //This stuff pertains to Try Again page.
 var octoX = 100; // This is the variable for the message and octocat's x-position. 
-var octoY = 400;  // This is the varialbe for the message and octocat's y-position. 
+var octoY = 400; // This is the varialbe for the message and octocat's y-position. 
 var bubButX = 360; // This is the variable for the bubble message's x-position. 
 var bubButY = 420; // This is the variable for the bubble message's y-position. 
 var aspectRatio = 201 / 264
-
 
 function preload() {
   cat = loadImage("octocat_sad.png"); // This loads in the image of the Octocat. Used the png file because 
@@ -31,14 +33,20 @@ function setup() {
 
 function draw() {
   background(230);
-  image(cat, octoX + 350, octoY - 300, 300, 300 * aspectRatio); // This calls the image at the defined
-  // x, y position. 
-  firstPage(); 
-  tryAgainPage();
+  firstPage();
+  if (page == 0) {
+   if (mouseIsPressed && mouseX > butX+250 && mouseX < butX+470 && mouseY > butY && mouseY < butY+50) {
+     
+    tryAgainPage(); 
+    }
+  } else {
+    firstPage();
+    }
 }
 
 /////////////////////////////// START OF FIRST PAGE CODE ///////////////////////////////
 
+// This function creates the entire first page. 
 function firstPage() {
   message(mesX, mesY);
   buttons(butX, butY);
@@ -69,12 +77,14 @@ function buttons() {
 
 /////////////////////////////// START OF TRY AGAIN PAGE CODE ///////////////////////////////
 
-// This is the function for the message shown on the page.
-function tryAgainPage(){
+// This is the function for the entire try again page. 
+function tryAgainPage() {
+  image(cat, octoX + 350, octoY - 300, 300, 300 * aspectRatio); // This calls the image at the defined
   tryAgainMessage(octoX, octoY); // This calls the message function at the defined x, y position. 
   tryAgainButton(bubButX, bubButY); // This calls the button function at the defined x, y position. 
 }
 
+// This is the function for the message shown on the page.
 function tryAgainMessage() {
   textSize(40);
   fill(0, 102, 153);
